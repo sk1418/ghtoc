2a07affcb24defb651d16189b6d63e5a686812fa
Kent@LsyHP <kent.yuan@Gmail.com>
2014-12-01 14:30:18 +0100

    init commit

---
+++ test/test.md
@@ -0,0 +1,10 @@
+### 3 hashes as top level
+#### sub level1-1 with special " ' & #'"
+#### sub level1-2 (parentheses)
+#####sub 1-2-1
+#####sub 1-2-2 <foo>
+### 3 hashes as top level2
+#### sub level2-1 with special " ' & #'"
+#### sub level2-2 (parentheses)
+#####sub 2-2-1
+#####sub 2-2-2 <foo>
---
+++ toc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import urllib
+import re
+
+top_level=77
+lnk_temp='%s- [%s](#%s)'
+
+def load_md_file(fname):
+    """load md content"""
+    lines = []
+    with open(fname, 'r') as file:
+        lines = file.readlines()
+    return lines
+
+def generate_toc(lines):
+    global top_level
+    headers = [e.strip() for e in lines if re.match(r'#+', e)]
+    #find top_level
+    for i,h in enumerate(headers):
+        ln = len(re.search(r'^#+',h).group(0))
+        top_level = ln if ln < top_level else top_level
+        headers[i] = re.sub(r'^#+\s*', str(ln)+' ', h)
+    headers = [tr_header(h) for h in headers]
+    print '\n'.join(headers)
+
+def tr_header(header):
+    global lnk_temp
+    lvl, txt = re.findall(r'^(\d+) (.*)', header)[0]
+    return lnk_temp%((int(lvl)-top_level)*'\t', txt, re.sub(' ','-',re.sub('[^-a-z ]','',txt.lower())))
+
+if __name__ == '__main__':
+    generate_toc(load_md_file('test/test.md'))
